27b9fa3e8bd872d3cb07a27d3692588fec400e74
xfakir <490679728@qq.com>
2020-10-07 19:21:53 +0800

    init

---
+++ src/main/java/cn/xfakir/xmall/XmallApplication.java
@@ -0,0 +1,13 @@
+package cn.xfakir.xmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/xfakir/xmall/XmallApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xfakir.xmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
