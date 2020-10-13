2483eecca146f0c764fe44e1a6afd727ba0f0954
xfakir <490679728@qq.com>
2020-10-13 21:08:13 +0800

    security

---
+++ xmall-security/src/main/java/cn/xfakir/xmall/security/XmallSecurityApplication.java
@@ -0,0 +1,13 @@
+package cn.xfakir.xmall.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmallSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmallSecurityApplication.class, args);
+    }
+
+}
---
+++ xmall-security/src/test/java/cn/xfakir/xmall/security/XmallSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xfakir.xmall.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmallSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
